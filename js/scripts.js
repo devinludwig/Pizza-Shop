6cf624c178fc7dfef8e1f1fb2ebe10d1d2b5a5f0
Devin Ludwig <devinludwig@gmail.com>
2016-11-04 12:04:19 -0700

    style page and add toppings/meal tax to price evaluator prototype; display choice of pizza with price determined by prototype.

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,56 @@
-function Pizza(toppings, size, sauce) {
+function Pizza(size, sauce, toppings, specialty) {
   this.toppings = toppings;
+  this.specialty = specialty;
   this.pizzaSize = size;
   this.sauce = sauce;
   this.price;
 }
 
 Pizza.prototype.evaluatePrice = function() {
-  if (this.pizzaSize === "large") {
+  if (this.pizzaSize === "Large") {
     this.price = 20;
-  } else if (this.pizzaSize === "medium") {
+  } else if (this.pizzaSize === "Medium") {
     this.price = 15;
   } else {
     this.price = 10;
   };
-  if (this.sauce === "red") {
+  if (this.sauce === "Red Marinara") {
     this.price = this.price + 1;
-  } else if (this.sauce === "white") {
+  } else if (this.sauce === "White Garlic Ricotta") {
     this.price = this.price + 2;
   } else {
     this.price = this.price + 3;
   };
-  
-}
+  this.price = 1.0725 * (this.price + this.toppings.length + this.specialty.length * 2);
+};
 
-$(function() {
+$(document).ready(function() {
   $('form').submit(function(event) {
-    // debugger;
-    event.preventDefault;
-    var toppingsArray = []
+    event.preventDefault();
+    $("#display , ul").empty();
+    var toppingsArray = [];
+    var specialtyArray = [];
     var size = $("#size").val();
     var sauce = $("#sauce").val();
-    $("input:checkbox[type='checkbox']:checked").each(function(){
+    $("input:checkbox[name='toppings']:checked").each(function(){
         toppingsArray.push($(this).val());
-        console.log(toppingsArray, size, sauce);
     });
-    var pizza = new Pizza(toppingsArray, size, sauce);
+    $("input:checkbox[name='specialty']:checked").each(function(){
+        specialtyArray.push($(this).val());
+    });
+    var pizza = new Pizza(size, sauce, toppingsArray, specialtyArray);
     pizza.evaluatePrice();
     console.log(pizza);
+    $("#display").append("You've ordered a " + pizza.pizzaSize + " with " + pizza.sauce + " sauce.<br>");
+    if (toppingsArray.length > 0 || specialtyArray.length > 0) {
+      $("#display").append("Toppings:");
+    };
+    for (var index = 0; index < toppingsArray.length; index ++) {
+      $("ul").append("<li>" + toppingsArray[index] + "</li>")
+    };
+    for (var index = 0; index < specialtyArray.length; index ++) {
+      $("ul").append("<li>" + specialtyArray[index] + "</li>")
+    };
+    $("#price").text('Your total is ' + pizza.price.toLocaleString('en-US', { style: 'currency', currency: 'USD' }));
   });
 });
